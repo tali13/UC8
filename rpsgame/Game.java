c69dcaf66a51b7ab30fc3d5be0b5469ec3709ea2
tali <talitanaibertdacosta@gmail.com>
2025-02-21 16:17:57 -0300

    Update Game.java

--- rpsgame/Game.java
+++ rpsgame/Game.java
@@ -10,28 +10,30 @@ public class Game {
  
     public void start() {
         
-       
+            
+        user = new player();
+        
+      while (true) {
+            System.out.println("Escolha: 1 (Pedra), 2 (Papel), 3 (Tesoura)");
+            int playerChoice = scanner.nextInt();
 
-    private RPSValidator Validator = new RPSValidator(); 
-    private Scanner scanner = new Scanner(System.in);
-  
-private player user;
+          
+            int computerChoice = (int) (Math.random() * 3) + 1; 
 
-    public void start() {
-     user = new player();
-        System.out.println("Bem-vindo ao RPS o jogo Pedra, Papel e Tesoura!");
-        System.out.println("Escolha 1 para Pedra, 2 para Papel, 3 para Tesoura:");
+           
+            if (Validator.validateChoice(playerChoice, computerChoice)) {
+                System.out.println("Você ganhou, parabéns :)");
+            } else {
+                System.out.println("Você perdeu, tente de novo mais tarde. :( ");
+            }
 
-        int playerChoice = scanner.nextInt();
-        int computerChoice = (int) (Math.random() * 3) + 1; 
- 
-    
-        if (Validator.validateChoice(playerChoice, computerChoice)) {
-            System.out.println("Você ganhou,parabens :)");
-        } else {
-            System.out.println("Você perdeu tente de novo mais tarde. :( ");
-        }
-
-        System.out.println("Computador escolheu: " + Validator.choiceToString(computerChoice));
+            
+            System.out.println("Deseja jogar novamente? (s/n)");
+            String resposta = scanner.next();
+            if (resposta.equalsIgnoreCase("n")) {
+                break;
     }
+}
+    }
+    
 }
