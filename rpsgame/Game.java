00c99ddd1f7c2b97f5b89b14deedefe8df283ab7
tali <talitanaibertdacosta@gmail.com>
2025-02-21 16:09:28 -0300

    Update Game.java

--- rpsgame/Game.java
+++ rpsgame/Game.java
@@ -7,19 +7,24 @@ package com.mycompany.rpsgame;
  * @author TALITANAIBERTDACOSTA
  */
 public class Game {
-   
-
+ 
+    public void start() {
+        
+       
 
     private RPSValidator Validator = new RPSValidator(); 
     private Scanner scanner = new Scanner(System.in);
+  
+private player user;
 
     public void start() {
+     user = new player();
         System.out.println("Bem-vindo ao RPS o jogo Pedra, Papel e Tesoura!");
         System.out.println("Escolha 1 para Pedra, 2 para Papel, 3 para Tesoura:");
 
         int playerChoice = scanner.nextInt();
         int computerChoice = (int) (Math.random() * 3) + 1; 
-
+ 
     
         if (Validator.validateChoice(playerChoice, computerChoice)) {
             System.out.println("Você ganhou,parabens :)");
