307d598bf9e95364691c89c551225090065ee420
tali <talitanaibertdacosta@gmail.com>
2025-02-21 16:08:10 -0300

    Update RpsGame.java

--- rpsgame/RpsGame.java
+++ rpsgame/RpsGame.java
@@ -11,8 +11,8 @@ package com.mycompany.rpsgame;
 public class RpsGame {
 
     public static void main(String[] args) {
-       player Player = new player();
-        player.start();
+       Game game = new Game();
+        game.start();
     }
     
 }
