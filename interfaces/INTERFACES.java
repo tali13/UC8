835f790fd9d7d1cf263064d1ae776291ca87047a
tali <talitanaibertdacosta@gmail.com>
2025-02-21 17:00:59 -0300

    Add files via upload

---
+++ interfaces/INTERFACES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaces;
+
+/**
+ *
+ * @author TALITANAIBERTDACOSTA
+ */
+public class INTERFACES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
