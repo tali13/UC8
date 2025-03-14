338c43cba18c9f6b6cedc5e91b046035dd0a4b6d
tali <talitanaibertdacosta@gmail.com>
2025-03-14 13:43:00 -0300

    Add files via upload

---
+++ interfacegrafica/InterfaceGrafica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfacegrafica;
+
+/**
+ *
+ * @author TALITANAIBERTDACOSTA
+ */
+public class InterfaceGrafica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
