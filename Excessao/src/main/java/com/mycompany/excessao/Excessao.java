471cb97d2b12863984893d841785505ab8574f47
tali <talitanaibertdacosta@gmail.com>
2025-03-31 13:36:23 -0300

    Add files via upload

---
+++ Excessao/src/main/java/com/mycompany/excessao/Excessao.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.excessao;
+
+/**
+ *
+ * @author TALITANAIBERTDACOSTA
+ */
+public class Excessao {
+
+    public static void main(String[] args) {
+       /* try{
+            int resultado = 10/0;
+            System.out.println(resultado);
+
+        }catch(Exception error){
+            System.out.println("error:"+ error.getMessage());
+        }
+        System.out.println("bloco a ser executado");
+    }
+
+    public static void VerificarIdade(int idade){
+        
+        if(idade<18){
+            
+            throw new IllegalArgumentException("deve ser mais de idade");
+        }
+        System.out.println("acesso permitido");
+    }
+*/
+    /*
+try {
+            Excessao fileChooser = new Excessao("arquivo.txt");
+            // ... your code to work with the file ... 
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar abrir o arquivo: " + e.getMessage());
+        }  
+*/
+    
+    
+    
+    
+    
+    }
+}
---
+++ exercicioArrayList/src/main/java/com/mycompany/exercicioarraylist/ExercicioArrayList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicioarraylist;
+
+/**
+ *
+ * @author TALITANAIBERTDACOSTA
+ */
+public class ExercicioArrayList {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
